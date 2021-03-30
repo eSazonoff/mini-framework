143e9aa9bd7ad5ee844b6476358af8f6ca5daba1
SALONBLOOM54 <klok-nikita@mail.ru>
2021-03-30 22:07:27 +0300

    Commit message.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
-     echo 'Start';
+
+	$query = $_SERVER['QUERY_STRING'];
+
+	require '../vendor/core/Router.php';
+
+	Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+
+	print_r(Router::getRoutes());
+
 ?>
